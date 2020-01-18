b7042757c6451d03cbd05363fcae3cbb391e49cf
Eugen Ciur <eugen@django-lessons.com>
2020-01-18 19:15:07 +0100

    quill dep included

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import $ from "jquery";
 import 'bootstrap';
+import Quill from 'quill';
 // js code here
+
+let article = document.getElementById('id_article');
+let editor;
+
+let options = {
+  placeholder: 'Compose an epic...',
+  theme: 'snow'
+};
+
+if (article) {
+    editor = new Quill(article, options);
+} else {
+    console.log("Article not found");
+};
---
+++ static/lessons/js/article.js
@@ -0,0 +1,4 @@
+(function($) {
+    var container = $('#id_article');
+    var editor = new Quill(container);
+})(django.jQuery);
