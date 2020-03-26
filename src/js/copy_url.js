b5c89ceb3e7f0ef3f851311545cbe3d17a0bd310
Eugen Ciur <eugen@django-lessons.com>
2020-03-26 08:00:02 +0100

    now with copy_url function

---
+++ src/js/copy_url.js
@@ -0,0 +1,13 @@
+
+function copy_url() {
+    let dummy = document.createElement('input'),
+        text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
+
+module.exports = copy_url;
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import $ from "jquery";
 import 'bootstrap';
-
+import copy_url from "./copy_url";
 
 
 $(function () {
-  $('#tab li:first-child a').tab('show')
+  $('#tab li:first-child a').tab('show');
+  $('#copy-url').click(function(e) { copy_url(); })
 })
